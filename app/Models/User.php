4f90f84ec96e80fd09f96d7223aa2ca4e46094d3
assulkhaniproject <assulkhaniproject@gmail.com>
2023-02-05 22:18:51 +0700

    view avatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,21 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\HasAvatar;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements HasAvatar
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
+    public function getFilamentAvatarUrl(): ?string
+    {
+        return 'storage/' . $this->avatar;
+    }
     /**
      * The attributes that are mass assignable.
      *
